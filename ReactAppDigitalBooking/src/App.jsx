f716dd5a5ed0041ba61f5f34448c76cc7e90e171
karencofone <cofonekaren97@gmail.com>
2023-02-16 16:39:50 -0300

    arreglo carpetas y path de rutas

---
+++ ReactAppDigitalBooking/src/App.jsx
@@ -0,0 +1,26 @@
+import './index.css'
+import Header from './Components/Header'
+import Home from '../src/Routes/Home'
+import LogIn from './Routes/LogIn'
+import CreateAcc from './Routes/CreateAcc'
+import Footer from './Components/Footer'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+      <Routes>
+        {/* <Routes> */}
+          <Route exact path='/' element={<Home/>}/>
+          <Route path='/login' element={<LogIn/>}/>
+          <Route path='/createacc' element={<CreateAcc/>}/>
+        {/* </Routes> */}
+      </Routes>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
---
+++ ReactAppDigitalBooking/src/Components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Logo from '../images/LogoHeader.png'
+import {Link} from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <div className='header'>
+       <Link to={'/'}><img src={Logo} alt="Logo"/></Link>
+       <div className='buttons'>
+        <Link to={'/login'}><button className='headerButtons'>Iniciar Sesion</button></Link>
+        <Link to={'createacc'}><button className='headerButtons'>Crear Cuenta</button></Link>
+       </div>
+    </div>
+  )
+}
+
+export default Header
